b2a335920c72ed9e94790efb47ed15384120ebd1
Madhupprasad <madhupprasad7@gmail.com>
2026-06-08 10:47:03 +0530

    Initial Commit

---
+++ mood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mood
+//
+//  Created by Madhupprasad S on 08/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mood/moodApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodApp.swift
+//  mood
+//
+//  Created by Madhupprasad S on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct moodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
